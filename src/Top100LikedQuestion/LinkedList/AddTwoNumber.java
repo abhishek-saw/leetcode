8a94a0cb530debd798585eadc418489e60283738
apple <abhishekggs7@gmail.com>
2024-04-02 13:52:40 +0530

    DAY 2 : 2 Qs

---
+++ src/Top100LikedQuestion/LinkedList/AddTwoNumber.java
@@ -0,0 +1,69 @@
+package LinkedList;
+
+public class AddTwoNumber {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        ListNode l11 = new ListNode(4);
+        ListNode l12 = new ListNode(3);
+        l1.next = l11;
+        l11.next = l12;
+        ListNode l2 = new ListNode(5);
+        ListNode l21 = new ListNode(6);
+        ListNode l22 = new ListNode(4);
+        l2.next = l21;
+        l21.next = l22;
+        print(l1);
+        print(l2);
+        ListNode answer = addTwoNumbers(l1,l2);
+        print(answer);
+    }
+
+    private static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode answer = null;
+        ListNode answerTail = null;
+        int sum;
+        int carry = 0;
+        while(l1!=null || l2!=null ) {
+            sum = carry;
+            carry =0;
+            if(l1!=null) {
+                sum = sum + l1.val;
+                l1 = l1.next;
+            }
+            if(l2!=null){
+                sum = sum + l2.val;
+                l2 = l2.next;
+            }
+            if(sum>9){
+                carry=sum/10;
+            }
+            if(answer==null){
+                answer = new ListNode(sum%10);
+                answerTail = answer;
+            }
+            else {
+                answerTail.next = new ListNode(sum % 10);
+                answerTail = answerTail.next;
+            }
+        }
+        if(carry!=0){
+            answerTail.next = new ListNode(carry);
+        }
+        return answer;
+    }
+    private static void print(ListNode l){
+        if(l==null){
+            System.out.println();
+            return;
+        }
+        System.out.print(l.val + " ") ;
+        print(l.next);
+    }
+}
