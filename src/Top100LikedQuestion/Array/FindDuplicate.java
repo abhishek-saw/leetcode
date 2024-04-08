cfb9c396b91d6e30073849f6e6a4d01f1dc372b6
apple <abhishekggs7@gmail.com>
2024-04-08 23:23:46 +0530

    DAY 8 : 3 Qs

---
+++ src/Top100LikedQuestion/Array/FindDuplicate.java
@@ -0,0 +1,23 @@
+package Array;
+
+import java.util.HashMap;
+
+public class FindDuplicate {
+    public static void main(String[] args) {
+        int [] nums = {1,3,4,2,2};
+        System.out.println(findDuplicate(nums));
+    }
+
+    private static int findDuplicate(int[] nums) {
+        HashMap<Integer,Integer> integerHashMap = new HashMap<>();
+        for(Integer integer: nums){
+            if(integerHashMap.containsKey(integer)){
+                return integer;
+            }
+            else{
+                integerHashMap.put(integer,1);
+            }
+        }
+        return -1;
+    }
+}
