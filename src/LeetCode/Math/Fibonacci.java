f0405f46f3fc362a5805105a3b735fddeccda2c8
apple <abhishekggs7@gmail.com>
2024-04-13 23:02:39 +0530

    DAY 13 : 2 Qs

---
+++ src/LeetCode/Math/Fibonacci.java
@@ -0,0 +1,18 @@
+package Math;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 2;
+        System.out.println(fibonacci(n));
+    }
+
+    private static int fibonacci(int n) {
+        int [] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+}
