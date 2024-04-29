3e7a81e20fc951e6f2ea419f68c1afdd5db160e2
apple <abhishekggs7@gmail.com>
2024-04-29 21:16:02 +0530

    DAY 28 : 1 Qs

---
+++ src/LeetCode/Array/ContainsDuplicate.java
@@ -0,0 +1,21 @@
+package Array;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int [] nums = {1,2,3,1};
+        System.out.println(containsDuplicate(nums));
+    }
+
+    private static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> map = new HashSet<>();
+        for (int num : nums) {
+            if (map.contains(num)) {
+                return true;
+            }
+            map.add(num);
+        }
+        return false;
+    }
+}
