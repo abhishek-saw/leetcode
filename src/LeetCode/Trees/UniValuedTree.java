525552ceeaaa141a56dc99ce870e0a8aaf2bd29b
apple <abhishekggs7@gmail.com>
2024-04-29 21:15:55 +0530

    DAY 27 : 1 Qs

---
+++ src/LeetCode/Trees/UniValuedTree.java
@@ -0,0 +1,40 @@
+package Trees;
+
+public class UniValuedTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode rL = new TreeNode(1);
+        TreeNode rR = new TreeNode(1);
+        TreeNode rLL = new TreeNode(1);
+        TreeNode rLR = new TreeNode(1);
+        TreeNode rRR = new TreeNode(1);
+        root.left = rL;
+        root.right = rR;
+        rL.left = rLL;
+        rL.right = rLR;
+        rR.right = rRR;
+        System.out.println(isUnivalTree(root));
+    }
+
+    private static boolean isUnivalTree(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        else {
+            return isUnivalTreeHelper(root,root.val);
+        }
+    }
+
+    private static boolean isUnivalTreeHelper(TreeNode root, int val) {
+        if(root== null){
+            return true;
+        }
+        if(root.val!=val){
+            return false;
+        }
+        else {
+            return isUnivalTreeHelper(root.left,val) &&  isUnivalTreeHelper(root.right,val) ;
+        }
+
+    }
+}
