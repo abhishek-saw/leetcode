8ddf2147018fede558d85c4ebcdf81a77a7c6156
apple <abhishekggs7@gmail.com>
2024-05-03 19:23:49 +0530

    DAY 31 : 1 Qs

---
+++ src/LeetCode/String/ReversePrefix.java
@@ -0,0 +1,16 @@
+package String;
+
+public class ReversePrefix {
+    public static void main(String[] args) {
+        String word  = "abcdefd";
+        char ch = 'd';
+        System.out.println(reversePrefix(word,ch));
+    }
+    public static String reversePrefix(String word, char ch) {
+        int j = word.indexOf(ch);
+        if (j != -1) {
+            return new StringBuilder(word.substring(0, j + 1)).reverse() + word.substring(j + 1);
+        }
+        return word;
+    }
+}
