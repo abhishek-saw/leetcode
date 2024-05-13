6775d81a9eee715554809c3cb23bfe8053124ba4
apple <abhishekggs7@gmail.com>
2024-05-14 00:01:32 +0530

    DAY 43 : 2 Qs

---
+++ src/LeetCode/Array/SearchMatrixII.java
@@ -0,0 +1,27 @@
+package Array;
+
+
+import static HashMaps.SetZeroes.print;
+
+public class SearchMatrixII {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}};
+        int target = 5;
+        print(matrix);
+        System.out.println(searchMatrix(matrix, target));
+    }
+    private static boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0;
+        int j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+}
