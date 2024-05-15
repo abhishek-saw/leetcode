1e925f2218da3c5c16a0e517f71a5ff71161f78a
apple <abhishekggs7@gmail.com>
2024-05-15 21:14:20 +0530

    DAY 45 : 2 Qs

---
+++ src/LeetCode/Array/PivotElement.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class PivotElement {
+    public static void main(String[] args) {
+        int [] nums = {1,7,3,6,5,6};
+        System.out.println(pivotIndex(nums));
+    }
+    private static int pivotIndex(int[] nums) {
+        int rightSum = 0;
+        for(Integer integer:nums) {
+            rightSum += integer;
+        }
+        int leftSum = 0;
+        for (int i=0;i<nums.length;i++){
+            rightSum-=nums[i];
+            if (leftSum==rightSum){
+                return i;
+            }
+            leftSum+=nums[i];
+        }
+        return -1;
+    }
+}
