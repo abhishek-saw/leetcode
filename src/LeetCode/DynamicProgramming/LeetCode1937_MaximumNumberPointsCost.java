33f7faf8eee7554708646c0c7368f22cd5054eae
apple <abhishekggs7@gmail.com>
2024-08-19 20:54:58 +0530

    DAY 139: 1

---
+++ src/LeetCode/DynamicProgramming/LeetCode1937_MaximumNumberPointsCost.java
@@ -0,0 +1,14 @@
+package DynamicProgramming;
+
+public class LeetCode1937_MaximumNumberPointsCost {
+    public static void main(String[] args) {
+        int[][] points1 = {{1,2,3},{1,5,1},{3,1,1}};
+        int[][] points2 = {{1,5},{2,3},{4,2}};
+        System.out.println(maxPoints(points1));
+        System.out.println(maxPoints(points2));
+    }
+
+    private static long maxPoints(int[][] points) {
+        return 0;
+    }
+}
