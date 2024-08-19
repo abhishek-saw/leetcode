9f5b5e4a9df6b9905af6729e74a02bbbbae41be1
apple <abhishekggs7@gmail.com>
2024-08-19 20:54:45 +0530

    DAY 138: 1

---
+++ src/LeetCode/Array/LeetCode624_MaximumDistanceArrays.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class LeetCode624_MaximumDistanceArrays {
+    public static void main(String[] args) {
+        int[][] arrays1 = {{1,2,3},{4,5},{1,2,3}};
+        int[][] arrays2 = {{1},{1}};
+        System.out.println(maxDistance(arrays1));
+        System.out.println(maxDistance(arrays2));
+    }
+
+    private static int maxDistance(int[][] arrays) {
+        return 0;
+    }
+}
