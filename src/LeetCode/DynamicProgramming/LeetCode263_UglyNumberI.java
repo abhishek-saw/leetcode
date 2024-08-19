3d0ce3481c4c124c2ee55d9ba4bd782c4934b344
apple <abhishekggs7@gmail.com>
2024-08-19 20:55:18 +0530

    DAY 140: 2

---
+++ src/LeetCode/DynamicProgramming/LeetCode263_UglyNumberI.java
@@ -0,0 +1,14 @@
+package DynamicProgramming;
+
+public class LeetCode263_UglyNumberI {
+    public static void main(String[] args) {
+       int n1 = 10;
+       int n2 = 1;
+        System.out.println(isUgly(n1));
+        System.out.println(isUgly(n2));
+    }
+
+    private static boolean isUgly(int n) {
+        return false;
+    }
+}
---
+++ src/LeetCode/DynamicProgramming/LeetCode264_UglyNumberII.java
@@ -0,0 +1,14 @@
+package DynamicProgramming;
+
+public class LeetCode264_UglyNumberII {
+    public static void main(String[] args) {
+       int n1 = 10;
+       int n2 = 1;
+        System.out.println(nthUglyNumber(n1));
+        System.out.println(nthUglyNumber(n2));
+    }
+
+    private static int nthUglyNumber(int n) {
+        return 0;
+    }
+}
